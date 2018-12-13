60585db36d53b3730d0ba1a1e4c6b7e62ede09c3
Zhenyu Li <775087535@qq.com>
2018-12-14 01:25:11 +0800

    Add Swiper components

---
+++ Swiper.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="swiper-container">
+        <div class="swiper-wrapper" ref="swiperContainer">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+
+export default {
+    name: 'swiper',
+    props: {
+        containerClass: {
+            default: 'swiper-container'
+        }
+    },
+    mounted() {
+        new Swiper(`.${this.containerClass}`, {
+            slidesPerView: 1,
+        })
+    }
+}
+</script>
+
+<style>
+.swiper-container {
+    width: 100%;
+    overflow: hidden;
+}
+
+.swiper-wrapper {
+    white-space: nowrap;
+}
+</style>
---
+++ SwiperItem.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'swiper-item',
+    render() {
+        return this.$slots.default[0]
+    },
+    mounted() {
+        this.$el.classList.add('swiper-slide')
+    }
+}
+</script>
